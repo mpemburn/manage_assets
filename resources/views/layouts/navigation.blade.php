929e19ecf569e0b910198c632fbf8aff6ebb32c7
Mark Pemburn <mark@pemburn.com>
2021-01-11 14:38:53 -0500

    Incorporate report page into Blade layout.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('report')" :active="request()->routeIs('report')">
+                        {{ __('Reports') }}
+                    </x-nav-link>
                 </div>
             </div>
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ReportController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,4 +22,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('/report', ReportController::class . '@show')->name('report');
+
 require __DIR__.'/auth.php';
+
