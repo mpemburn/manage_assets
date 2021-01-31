366cdbe2c2284f544f4941fd70c85d259a8c94cc
Mark Pemburn <mark@pemburn.com>
2021-01-31 13:12:43 -0500

    Add header code to ReportIssue class.

--- app/Models/ReportIssue.php
+++ app/Models/ReportIssue.php
@@ -10,16 +10,37 @@ class ReportIssue extends Model
 {
     use HasFactory;
 
+    public const HEADER_ARRAY = [
+        'severity' => 'Severity',
+        'problem' => 'Problem',
+        'description' => 'Problem Description',
+        'solution' => 'Solution'
+    ];
+
     public $fillable = [
         'report_id',
         'severity',
         'problem',
+        'description',
         'solution',
         'uid',
     ];
 
+    public static function map(array $values): array
+    {
+        return array_combine(
+            array_keys(self::HEADER_ARRAY),
+            $values
+        );
+    }
+
+    public static function getIssueHeaderValues(): array
+    {
+        return array_values(self::HEADER_ARRAY);
+    }
+
     public function reportLines(): HasMany
     {
-        return $this->hasMany(ReportLine::class, 'issue_id');
+        return $this->hasMany(ReportLine::class, 'report_issue_id');
     }
 }
