38ac5253d9a983aa652e8381662f9ef884e9ecb3
Mark Pemburn <mark@pemburn.com>
2021-01-31 13:12:15 -0500

    Add fillable array and improve getCreatedAtAttribute method in Report class.

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -2,16 +2,31 @@
 
 namespace App\Models;
 
+use Exception;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Log;
 
 class Report extends Model
 {
     use HasFactory;
 
+    public $fillable = [
+        'uid',
+        'file_name'
+    ];
+
     public function getCreatedAtAttribute($value): string
     {
-        return Carbon::parse($value)->format('n-j-Y g:i');
+        try {
+            return Carbon::parse($value)
+                ->timezone(env('TIMEZONE'))
+                ->format('n-j-Y g:i A');
+        } catch (Exception $e) {
+            Log::debug('getCreatedAtAttribute error: ' . $e->getMessage());
+        }
+
+        return $value;
     }
 }
