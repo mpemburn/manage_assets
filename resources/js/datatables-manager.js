d11bb9f603640f9b7da48282f8c3734213aecce9
Mark Pemburn <mark@pemburn.com>
2021-02-06 09:44:16 -0500

    Modify page-length dropdown.

--- resources/js/datatables-manager.js
+++ resources/js/datatables-manager.js
@@ -2,7 +2,10 @@ export default class DatatablesManager {
     constructor() {
         this.table = $('#inventory-table');
         this.table.DataTable({
-            pageLength: 250
+            pageLength: 250,
+            lengthMenu: [ 10, 25, 50, 75, 100, 250 ]
         });
+
+        $('[name="inventory-table_length"]').css({'width': '100px'});
     }
 }
