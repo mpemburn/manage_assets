ffbc18db6a63b9c9140a7b9282f7260d320fdc43
Mark Pemburn <mark@pemburn.com>
2021-03-10 11:27:12 -0500

    Add Events and Listeners for Roles and Permissions

--- app/Models/PermissionUi.php
+++ app/Models/PermissionUi.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Events\PermissionCreated;
 use App\Interfaces\UiInterface;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Spatie\Permission\Models\Permission;
@@ -9,4 +10,12 @@ use Spatie\Permission\Models\Permission;
 class PermissionUi extends Permission implements UiInterface
 {
     use HasFactory;
+
+    public function save(array $options = [])
+    {
+        parent::save($options);
+
+        event(new PermissionCreated());
+    }
+
 }
--- app/Models/RoleUi.php
+++ app/Models/RoleUi.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Events\RoleCreated;
 use App\Interfaces\UiInterface;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Spatie\Permission\Models\Role;
@@ -9,4 +10,11 @@ use Spatie\Permission\Models\Role;
 class RoleUi extends Role implements UiInterface
 {
     use HasFactory;
+
+    public function save(array $options = [])
+    {
+        parent::save($options);
+
+        event(new RoleCreated());
+    }
 }
