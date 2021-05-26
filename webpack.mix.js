c7d94869416eedda3604a88cffdd4c8a74c9b9d0
Mark Pemburn <mark@pemburn.com>
2021-05-26 07:24:59 -0400

    Install Vue and rolevue package.

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,21 +6,11 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
-module.exports = {
-    module: {
-        loaders : [
-            { test: /\.xml$/, loader: 'xml-loader' } // will load all .xml files with xml-loader by default
-        ]
-    },
-};
-
-mix.js('resources/js/*.js', 'public/js').postCss('resources/css/app.css', 'public/css', [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('autoprefixer'),
-]).version();
+mix.js('resources/js/app.js', 'public/js')
+    .vue()
+    .sass('resources/sass/app.scss', 'public/css');
