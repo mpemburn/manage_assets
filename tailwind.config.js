b60c3390cb898363f30004c198429a948afcd4bc
Mark Pemburn <mark@pemburn.com>
2021-05-27 07:28:43 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,25 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    purge: ['./storage/framework/views/*.php', './resources/views/**/*.blade.php'],
+    purge: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
 
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                "smoke-900": "rgba(0, 0, 0, 0.9)",
+                "smoke-800": "rgba(0, 0, 0, 0.75)",
+                "smoke-600": "rgba(0, 0, 0, 0.6)",
+                smoke: "rgba(0, 0, 0, 0.5)",
+                "smoke-400": "rgba(0, 0, 0, 0.4)",
+                "smoke-200": "rgba(0, 0, 0, 0.25)",
+                "smoke-100": "rgba(0, 0, 0, 0.1)",
             },
         },
     },
